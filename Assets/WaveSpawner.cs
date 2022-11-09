b03afaa7e135935ed2fceb3ab96fd9559e0038b4
Maks Wróblewski <maksymilianwroblewski8@gmail.com>
2022-11-09 11:26:25 +0100

    upgrade wavespawner system

--- Assets/WaveSpawner.cs
+++ Assets/WaveSpawner.cs
@@ -2,45 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+[System.Serializable]
+public class Wave
+{
+    public GameObject enemy;
+    public int enemyAmount;
+    public float rate;
+    public float timeToNextWave;
+}
 public class WaveSpawner : MonoBehaviour
 {
-    public Transform enemyPrefab;
-    public float timeBetweenWaves = 5f;
-    private float countDown = 2f;
-
-    private int waveIndex =01;
+    private float timetoStart = 3f;
+    public Wave[] waves;
+    private int waveIndex = 0;
     private Transform spawnPoint;
-    [SerializeField] private int waveCount;
 
 
     private void Start()
     {
         spawnPoint = GameObject.FindGameObjectWithTag("Start").transform;
-    }
-
-    private void Update()
-    {
-        if(countDown <= 0)
-        {
-            StartCoroutine(SpawnWave());
-            countDown = timeBetweenWaves;
-        }
-        countDown -= Time.deltaTime;
+        WaveStart();
     }
 
     IEnumerator SpawnWave()
     {
-        waveIndex++;
+        yield return new WaitForSeconds(timetoStart);
+        Wave wave = waves[waveIndex];
 
-        for (int i = 0; i < waveIndex; i++)
+        for (int i = 0; i < wave.enemyAmount; i++)
         {
-            SpawnEnemy();
-            yield return new WaitForSeconds(0.5f);
+            SpawnEnemy(wave.enemy);
+            yield return new WaitForSeconds(wave.rate);
+        }
+        yield return new WaitForSeconds(wave.timeToNextWave);
+        waveIndex++;
+        timetoStart = 0;
+        WaveStart();
+
+        if (waveIndex == waves.Length)
+        {
+            Debug.Log("wygrales");
+            this.enabled = false;
         }
     }
         
-    private void SpawnEnemy()
+    private void SpawnEnemy(GameObject enemy)
     {
-        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
+    }
+    private void WaveStart()
+    {
+        StartCoroutine(SpawnWave());
     }
 }
